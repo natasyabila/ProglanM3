ba284047d29b68dc6da618ba6a44c0f174216d00
natasyabila <natasyasalsabilawebmail@webmail.umm.ac.id>
2024-10-25 14:13:46 +0700

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,8 @@
-package com.example;
-
-import org.apache.commons.lang3.StringUtils;
-
-public class App {
+package org.example;
+public class Main {
     public static void main(String[] args) {
         String text = "Hello, Maven!";
-        String reversedText = StringUtils.reverse(text);
-        System.out.println(reversedText);
+
+        System.out.println(text);
     }
 }
